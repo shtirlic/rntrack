c8314b297bea02349ac6f5b50f8e8565e954c60f
dukelsky <dukelsky@7ed1541a-b14e-4b30-b938-c7354050dff3>
2020-04-22 13:01:28 +0000

    delete devel210 branch after reintegration with trunk

--- branches/devel210/devel/commits/makelst.pl
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/perl
-
-# Write the list of all modified files to ./commit.lst
-my @update = qx(svn status -q);
-chomp(@update);
-open(FH, ">", "./commit.lst") or die "Cannot open the file ./commit.lst: $!";
-map {print FH substr($_, 8)."\n";} @update;
-close(FH);
